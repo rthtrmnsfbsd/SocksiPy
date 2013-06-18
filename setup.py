b612ff860c750df814692ea9eff4d34a3f829fc0
Alexey Vishnevski <vishnevski@xtim.com>
2013-06-18 14:38:09 +0300

    Fix

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='SocksiPy',
+    version='1.00',
+    author='Dan-Haim',
+    author_email='negativeiq@users.sourceforge.net',
+    packages=find_packages(),
+    include_package_data=True,
+    url='http://pypi.python.org/pypi/SocksiPy/',
+    license='LICENSE',
+    description='SocksiPy',
+    long_description=open('README.txt').read(),
+)
